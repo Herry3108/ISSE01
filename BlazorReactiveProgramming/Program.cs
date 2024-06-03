61c8e818ef548730386ca806cb4f19760f4a652b
Benedikt Hering <benedikt.hering@iu-study.org>
2024-06-03 12:51:56 +0200

    False naming & init Blazor project new

--- BlazorReactiveProgramming/Program.cs
+++ BlazorReactiveProgramming/Program.cs
@@ -1,22 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorReactiveProgramming;
 using Fluxor;
-using blazor_reactive_programming;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddFluxor(o =>
 {
     o.ScanAssemblies(typeof(Program).Assembly);
     o.UseReduxDevTools(rdt =>
       {
-          rdt.Name = "Blazor reactive programming";
+          rdt.Name = "My application";
       }
     );
 });
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
