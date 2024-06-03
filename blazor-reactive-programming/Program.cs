b55f7f4fb478d1ebe2bc5e617ed4d631cdca44f7
Benedikt Hering <benedikt.hering@iu-study.org>
2024-06-03 12:34:49 +0200

    Configure application

--- blazor-reactive-programming/Program.cs
+++ blazor-reactive-programming/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Fluxor;
 using blazor_reactive_programming;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,15 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluxor(o =>
+{
+    o.ScanAssemblies(typeof(Program).Assembly);
+    o.UseReduxDevTools(rdt =>
+      {
+          rdt.Name = "Blazor reactive programming";
+      }
+    );
+});
 
 await builder.Build().RunAsync();
